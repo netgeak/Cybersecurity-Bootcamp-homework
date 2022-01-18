7da0a93b1dc0f1a37b511c61f2e02383e9ed615a
Netgeak - Carl Conn <netgeak@gmail.com>
2022-01-18 17:19:16 -0600

    Add files via upload

---
+++ Week 3/Scripts/#0310_dealer_script01.sh
@@ -0,0 +1,6 @@
+#1 /bin/bash/
+
+cat 0310_Dealer_schedule | awk '{print $1 $2,$5,$6}' > March03_dealer_schedule.txt #> 0310_dealer_schedule_clean.txt
+grep -l Billy March03_dealer_schedule.txt >> Dealers_working_during_losses
+grep $1 March03_dealer_schedule.txt >> Dealers_working_during_losses
+
