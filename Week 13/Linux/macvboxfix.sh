ae7cdfefee3d9e591d7bb9596bbe1b2cb3806f5e
Netgeak - Carl Conn <netgeak@gmail.com>
2022-03-28 21:23:51 -0500

    Update macvboxfix.sh

--- Week 13/Linux/macvboxfix.sh
+++ Week 13/Linux/macvboxfix.sh
@@ -4,5 +4,5 @@ mkdir /etc/vbox/
 touch /etc/vbox/networks.conf
 echo " * 0.0.0.0/0 ::/0" > /etc/vbox/networks.conf
 chmod 777 /etc/vbox/networks.conf
-# This fixes the network issue in virtualbox. This creates teh directory, the file, then adds the config that allows all subnets to be accepted then makes the file 
-# world accessible.
+# This fixes the network issue for virtualbox on Mac. This creates the directory, the file, then adds the config that allows all subnets to be accepted then makes the file 
+# world accessible. Created for the first capture the flag.
