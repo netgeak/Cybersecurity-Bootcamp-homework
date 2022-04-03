a79241a65233ea29ba7bdb02a310180e5bd3795c
Netgeak - Carl Conn <netgeak@gmail.com>
2022-04-02 21:09:43 -0500

    Update 0310_dealer_script01.sh

--- Week 13/Linux/0310_dealer_script01.sh
+++ Week 13/Linux/0310_dealer_script01.sh
@@ -1,4 +1,4 @@
-#1 /bin/bash/
+#! /bin/bash/
 
 cat 0310_Dealer_schedule | awk '{print $1 $2,$5,$6}' > March03_dealer_schedule.txt #> 0310_dealer_schedule_clean.txt
 grep -l Billy March03_dealer_schedule.txt >> Dealers_working_during_losses
