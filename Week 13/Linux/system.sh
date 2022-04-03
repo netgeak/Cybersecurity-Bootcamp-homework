f9e755b401d221539c2e4facd8d6503118a9d4df
Netgeak - Carl Conn <netgeak@gmail.com>
2022-04-02 21:11:56 -0500

    Add files via upload

---
+++ Week 13/Linux/system.sh
@@ -0,0 +1,10 @@
+#!/bin/bash/
+
+# Free Memory
+free -h > ~/backups/freemem/free_mem.txt
+# diskusage
+du -h > ~/backups/diskuse/disk_use.txt
+# open files
+lsof > ~/backups/openlist/open_list.txt
+# diskspace
+df -h > ~/backups/freedisk/free_disk.txt
